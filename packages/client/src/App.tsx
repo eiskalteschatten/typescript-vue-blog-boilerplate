6f1d9973820a5852433d9199b27b7e36af1cdc1a
Alex Seifert <alex@alexseifert.com>
2022-08-07 17:44:42 +0200

    Detect and save prefers dark mode changes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { FLWrapper } from '@alexseifert/frontend-library';
 
-function App() {
+import { useAppDispatch } from 'store/hooks';
+import { setWindowWidth, setPrefersDarkMode } from 'store/entities/ui';
+
+const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useLayoutEffect(() => {
+    dispatch(setPrefersDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches));
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      dispatch(setPrefersDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches));
+    });
+
+    const handleResize = () => dispatch(setWindowWidth(window.innerWidth));
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <FLWrapper>
       test
