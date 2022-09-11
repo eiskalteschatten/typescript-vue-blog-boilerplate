84ae12df71fafa034c275a9eb771de929c37bdd4
Alex Seifert <alex@alexseifert.com>
2022-09-11 14:34:30 +0200

    Fix a layout issue

--- packages/client/src/components/layouts/Main/sublayouts/Basic/index.tsx
+++ packages/client/src/components/layouts/Main/sublayouts/Basic/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import styles from './styles.module.scss';
-
 interface Props {
   children: React.ReactNode;
 }
 
 const BasicLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className={styles.layout}>
+    <div>
       {children}
     </div>
   );
