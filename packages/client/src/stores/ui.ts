f19c3f4237078eb968eeb988b8687455a8f64a61
Alex Seifert <alex@alexseifert.com>
2023-01-19 12:34:18 +0100

    Update types

--- packages/client/src/stores/ui.ts
+++ packages/client/src/stores/ui.ts
@@ -5,8 +5,8 @@ export const useUIStore = defineStore('ui', () => {
   const windowWidth = ref(window.innerWidth)
   const prefersDarkMode = ref(false)
   const isLoading = ref(false)
-  const globalInfo = ref<string>();
-  const globalError = ref<string>();
+  const globalInfo = ref<string | undefined>();
+  const globalError = ref<string | undefined>();
 
   return {
     windowWidth,
