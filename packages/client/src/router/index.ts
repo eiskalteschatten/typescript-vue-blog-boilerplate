e8afec02adfc732957e4d090b6619ee5b16945ae
Alex Seifert <alex@alexseifert.com>
2023-01-19 14:30:08 +0100

    Add the route guard for the login

--- packages/client/src/router/index.ts
+++ packages/client/src/router/index.ts
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
+import { useAccountStore } from '@/stores/account';
+
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
     },
     {
       path: '/about',
-      name: 'about',
+      name: 'About',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -20,10 +22,19 @@ const router = createRouter({
     },
     {
       path: '/login',
-      name: 'login',
+      name: 'Login',
       component: () => import('../views/AuthLoginView.vue')
     }
   ]
 })
 
 export default router
+
+router.beforeEach(async (to) => {
+  const { user, accessToken, refreshToken } = useAccountStore();
+  const userIsLoggedin = user && accessToken && refreshToken;
+
+  if (!userIsLoggedin && to.name !== 'Login' && to.name !== 'Register') {
+    return { name: 'Login' }
+  }
+})
