c837ebc00c5c71bbf9f6f52b650e40e2f973c1aa
Alex Seifert <alex@alexseifert.com>
2023-01-19 15:18:56 +0100

    Create the login form

--- packages/client/src/layouts/AuthLayout.vue
+++ packages/client/src/layouts/AuthLayout.vue
@@ -1,5 +1,15 @@
 <template>
-  <main class="d-flex w-100 h-100 align-items-center justify-content-center">
-    <slot></slot>
+  <main class="d-flex w-100 h-100 justify-content-center content">
+    <div class="card">
+      <div class="card-body">
+        <slot></slot>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+.content {
+  margin-top: 10vh;
+}
+</style>
--- packages/client/src/views/AuthLoginView.vue
+++ packages/client/src/views/AuthLoginView.vue
@@ -4,6 +4,30 @@ import AuthLayout from '@/layouts/AuthLayout.vue'
 
 <template>
   <AuthLayout>
-    Login
+    <div class="vstack gap-3 login">
+      <div>
+        <h3>Login</h3>
+      </div>
+
+      <div>
+        <label for="emailAddress" class="form-label">Email Address</label>
+        <input type="email" class="form-control" id="emailAddress" placeholder="name@example.com">
+      </div>
+
+      <div>
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password">
+      </div>
+
+      <div class="mt-3 text-center">
+        <button class="btn btn-primary">Log In</button>
+      </div>
+    </div>
   </AuthLayout>
 </template>
+
+<styles scoped lang="scss">
+  .login {
+    min-width: 300px;
+  }
+</styles>
